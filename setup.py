7288d36bb2ae8e5fd0b7a4c2522d139422cea3bf
Henri <henri.de.boutray@colibritd.com>
2024-02-27 14:07:27 +0100

    pipy image problems

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 long_description = long_description.replace(
     "resources/dark-logo.svg",
-    "https://raw.githubusercontent.com/ColibrITD-SAS/mpqp/main/resources/dark-logo.png",
+    "https://raw.githubusercontent.com/ColibrITD-SAS/mpqp/main/resources/dark-logo.svg",
 ).replace(
     "resources/mpqp-usage.gif",
     "https://raw.githubusercontent.com/ColibrITD-SAS/mpqp/main/resources/mpqp-usage.gif",
