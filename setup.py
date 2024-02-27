39036a3e8fdeb917c0c52312458c6f779fbce638
Henri <henri.de.boutray@colibritd.com>
2024-02-27 13:57:01 +0100

    exploration on the pipy image problem

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 long_description = long_description.replace(
     "resources/dark-logo.svg",
-    "https://github.com/ColibrITD-SAS/mpqp/blob/main/resources/dark-logo.png?raw=true",
+    "https://raw.githubusercontent.com/ColibrITD-SAS/mpqp/main/resources/mpqp-usage.png",
 ).replace(
     "resources/mpqp-usage.gif",
-    "https://github.com/ColibrITD-SAS/mpqp/blob/main/resources/mpqp-usage.png?raw=true",
+    "https://raw.githubusercontent.com/ColibrITD-SAS/mpqp/blob/main/resources/mpqp-usage.png",
 )
 
 with open("LICENSE", "r") as f:
