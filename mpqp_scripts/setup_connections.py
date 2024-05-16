799f4aef6503c0c309870ab623a3c0f53e43132e
JulienCalistoTD <164006610+JulienCalistoTD@users.noreply.github.com>
2024-05-16 14:50:43 +0200

    feat: add cirq to setup connection

--- mpqp_scripts/setup_connections.py
+++ mpqp_scripts/setup_connections.py
@@ -17,6 +17,7 @@ def print_config_info():
     import mpqp.execution.connection.aws_connection as awsc
     import mpqp.execution.connection.env_manager as env_m
     import mpqp.execution.connection.ibm_connection as ibmqc
+    import mpqp.execution.connection.google_connection as cirqc
     from mpqp.tools.errors import IBMRemoteExecutionError
 
     """Prints the info concerning each provider's registered account."""
@@ -41,6 +42,11 @@ def print_config_info():
             print("Account not configured")
         else:
             print("Error occurred when getting AWS account info.")
+    print("===== Cirq info : ===== ")
+    try:
+        print(cirqc.get_google_account_info())
+    except Exception as err:
+            print("Error occurred when getting Cirq account info.")
     input("Press 'Enter' to continue")
     return "", []
 
@@ -53,23 +59,44 @@ def main_setup():
     import mpqp.execution.connection.aws_connection as awsc
     import mpqp.execution.connection.ibm_connection as ibmqc
     import mpqp.execution.connection.qlm_connection as qlmc
+    import mpqp.execution.connection.google_connection as cirqc
     from mpqp.tools.choice_tree import AnswerNode, QuestionNode, run_choice_tree
 
+    def return_action():
+        return "", []
+    
     setup_tree = QuestionNode(
         "~~~~~ MPQP REMOTE CONFIGURATION ~~~~~",
         [
             AnswerNode("IBM configuration", ibmqc.setup_ibm_account),
             AnswerNode("QLM configuration", qlmc.setup_qlm_account),
             AnswerNode("Amazon Braket configuration", awsc.setup_aws_braket_account),
+            AnswerNode("Cirq configuration", return_action),
             AnswerNode("Config information", print_config_info),
         ],
     )
 
+    cirq_setup_tree = QuestionNode(
+        "~~~~~ Cirq REMOTE CONFIGURATION ~~~~~",
+        [
+            AnswerNode("↩", return_action),
+            AnswerNode("Ionq configuration", cirqc.config_ionq_account),
+        ],
+    )
+
     for answer in setup_tree.answers:
-        answer.next_question = setup_tree
+        if answer.label == "Cirq configuration":
+            answer.next_question = cirq_setup_tree
+        else:
+            answer.next_question = setup_tree
+
+    for answer in cirq_setup_tree.answers:
+        if answer.label == "↩":
+            answer.next_question = setup_tree
+        else:
+            answer.next_question = cirq_setup_tree
 
     run_choice_tree(setup_tree)
-
 
 if __name__ == "__main__":
     try:
