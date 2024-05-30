50cb686688c9c57b025f3641f7e07c3a5794a81f
JulienCalistoTD <164006610+JulienCalistoTD@users.noreply.github.com>
2024-05-30 10:38:14 +0200

    doc: rename 3M-TODO

--- mpqp/execution/connection/key_connection.py
+++ mpqp/execution/connection/key_connection.py
@@ -70,7 +70,7 @@ def test_ionq_connection(key: Optional[str] = None) -> bool:
 
 def config_aqt_key():
     """
-    #3M-TODO add aqt in list of provider
+    # 3M-TODO add aqt in list of provider
     Configure the AQT account by setting the API token.
 
     Returns:
@@ -83,7 +83,7 @@ def config_aqt_key():
 
 def test_aqt_connection(key: Optional[str] = None) -> bool:
     """
-    #3M-TODO install qiskit_aqt_provider by updating qiskit to 1.0.0
+    # 3M-TODO install qiskit_aqt_provider by updating qiskit to 1.0.0
     Test the connection to the AQT service.
 
     Returns:
@@ -118,7 +118,7 @@ def get_ionq_job_ids() -> list[str]:
 
 def get_aqt_job_ids() -> list[str]:
     """
-    #3M-TODO 
+    # 3M-TODO 
     Retrieves all job IDs associated with AQT jobs.
 
     Returns:
