fd57193f8487c884b43ee89fa2993ac12dcb32b9
Muhammad Attallah <muhammad.attallah@colibritd.com>
2024-06-24 13:20:13 +0200

    doc: update doc for parametrized gate, abc instantiation

--- mpqp/core/instruction/gates/parametrized_gate.py
+++ mpqp/core/instruction/gates/parametrized_gate.py
@@ -31,8 +31,10 @@ from mpqp.core.instruction.gates.gate_definition import GateDefinition
 
 @typechecked
 class ParametrizedGate(Gate, ABC):
-    """Abstract class to factorize behavior of parametrized gate. Since it's an
-    abstract class, do not instantiate one yourself.
+    """Abstract class to factorize behavior of parametrized gate.
+
+    .. note::
+        This class is abstract and should not be instantiated directly.
 
     Args:
         definition: Provide a definition of the gate (matrix, gate combination,
