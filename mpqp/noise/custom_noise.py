1cf1bcc269bfbd6f83c98873b0febe87daa229fa
Henri <henri.de.boutray@colibritd.com>
2024-07-25 17:40:21 +0200

    fix: various type problems

--- mpqp/noise/custom_noise.py
+++ mpqp/noise/custom_noise.py
@@ -1,15 +1,20 @@
-class CustomNoise():
+from typing import Sequence
+
+
+class CustomNoise:
     """# 3M-TODO : implement and comment"""
+
     pass
 
 
-class KrausOperator():
+class KrausOperator:
     """# 3M-TODO : implement and comment"""
+
     pass
 
 
-class KrausRepresentation():
+class KrausRepresentation:
     """# 3M-TODO : implement and comment"""
 
-    def __init__(self, k_ops: list[KrausOperator]):
-        self.pp = k_ops
+    def __init__(self, k_ops: Sequence[KrausOperator]):
+        self.pp = list(k_ops)
