a26a3d488dbb5c122b9ef801a4d74797b86c6e8a
Muhammad Attallah <muhammad.attallah@colibritd.com>
2024-08-08 11:34:03 +0200

    chore: identify available IBMDevice simulators that support noise

--- mpqp/execution/devices.py
+++ mpqp/execution/devices.py
@@ -87,8 +87,6 @@ class IBMDevice(AvailableDevice):
     AER_SIMULATOR_STABILIZER = "stabilizer"
     AER_SIMULATOR_EXTENDED_STABILIZER = "extended_stabilizer"
     AER_SIMULATOR_MATRIX_PRODUCT_STATE = "matrix_product_state"
-    # AER_SIMULATOR_UNITARY = "unitary"
-    # AER_SIMULATOR_SUPEROP = "superop"
 
     IBM_BRISBANE = "ibm_brisbane"
     IBM_OSAKA = "ibm_osaka"
@@ -137,12 +135,7 @@ class IBMDevice(AvailableDevice):
         return "simulator" in self.value
 
     def is_noisy_simulator(self) -> bool:
-        raise NotImplementedError()
-        # TODO: determine which devices can simulate noise or not for Qiskit
-        noise_support_devices = {
-            IBMDevice.AER_SIMULATOR_STABILIZER: True,
-        }
-        return self in noise_support_devices
+        return self.is_simulator()
 
 
 class ATOSDevice(AvailableDevice):
