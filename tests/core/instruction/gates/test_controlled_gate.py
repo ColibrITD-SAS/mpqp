1bcf93101779fc4735da764e9c50bed0fba4046f
Henri <henri.de.boutray@colibritd.com>
2024-09-20 11:44:17 +0200

    fix: type issue

--- tests/core/instruction/gates/test_controlled_gate.py
+++ tests/core/instruction/gates/test_controlled_gate.py
@@ -1,7 +1,6 @@
 from typing import Optional
 
 import numpy as np
-import numpy.typing as npt
 import pytest
 from qiskit.circuit import Parameter
 
@@ -46,5 +45,5 @@ class CustomControlledGate(ControlledGate):
         assert self.non_controlled_gate is not None
         return self.non_controlled_gate.to_other_language(language, qiskit_parameters)
 
-    def to_matrix(self, nb_qubits: int = 0) -> npt.NDArray[np.complex64]:
+    def to_matrix(self, desired_gate_size: int = 0):
         return np.array([[1, 0], [0, 1]], dtype=np.complex64)
