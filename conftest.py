20244507dc913c10049539f6b647ac8ee55a3f24
Muhammad Attallah <muhammad.attallah@colibritd.com>
2024-10-11 00:48:51 +0200

    chore: add global seed option

--- conftest.py
+++ conftest.py
@@ -1,10 +1,28 @@
-from pytest import Parser
+import pytest
 
 
-def pytest_addoption(parser: Parser):
+def pytest_addoption(parser: pytest.Parser):
     parser.addoption("--long", action="store_false", help="If set, long tests will run")
     parser.addoption(
         "--long-local",
         action="store_false",
         help="If set, only local long tests will run",
     )
+    parser.addoption(
+        "--seed",
+        action="store",
+        default=None,
+        help="Set a global random seed for tests (default is None for random behavior).",
+    )
+
+
+@pytest.fixture
+def global_seed(request):
+    seed = request.config.getoption("--seed")
+    if seed == 'None':
+        print("No seed is provided, using the default random behavior.")
+        return None
+
+    print(f"\nSetting global random seed to {seed}")
+
+    return int(seed) if seed is not None else None
