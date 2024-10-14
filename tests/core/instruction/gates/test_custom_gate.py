cd6a58fdbcb37f7991d52d533e5f5a6e6b51d688
Hamsatz <hamza.jaffali@colibritd.com>
2024-10-14 16:19:38 +0200

    test: fix precision for custom gate

--- tests/core/instruction/gates/test_custom_gate.py
+++ tests/core/instruction/gates/test_custom_gate.py
@@ -57,7 +57,8 @@ def test_random_orthogonal_matrix(circ_size: int, device: AvailableDevice):
     ):
         result = _run_single(c, device, {})
 
-    assert matrix_eq(result.amplitudes, exp_state_vector)
+    # we reduce the precision because of approximation errors coming from CustomGate usage
+    assert matrix_eq(result.amplitudes, exp_state_vector, 1e-5, 1e-5)
 
 
 @pytest.mark.parametrize(
@@ -101,7 +102,8 @@ def test_custom_gate_with_native_gates(device: AvailableDevice):
     ):
         result2 = _run_single(c2, device, {})
 
-    assert matrix_eq(result1.amplitudes, result2.amplitudes)
+    # we reduce the precision because of approximation errors coming from CustomGate usage
+    assert matrix_eq(result1.amplitudes, result2.amplitudes, 1e-5, 1e-5)
 
 
 @pytest.mark.parametrize(
@@ -131,4 +133,5 @@ def test_custom_gate_with_random_circuit(circ_size: int, device: AvailableDevice
         result1 = _run_single(random_circ, device, {})
         result2 = _run_single(custom_gate_circ, device, {})
 
-    assert matrix_eq(result1.amplitudes, result2.amplitudes)
+    # we reduce the precision because of approximation errors coming from CustomGate usage
+    assert matrix_eq(result1.amplitudes, result2.amplitudes, 1e-5, 1e-5)
