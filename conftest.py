b956e2d2656fac5762168e4c8394981f2c1f5709
Muhammad Attallah <muhammad.attallah@colibritd.com>
2024-10-21 16:09:20 +0200

    chore: update global_seed setting

--- conftest.py
+++ conftest.py
@@ -1,5 +1,3 @@
-from typing import Optional
-
 import pytest
 
 
@@ -14,26 +12,17 @@ def pytest_addoption(parser: pytest.Parser):
         "--seed",
         action="store",
         default=None,
+        type=int,
         help="Set a global random seed for tests (default is None for random behavior).",
     )
 
 
 @pytest.fixture
-def global_seed(request: pytest.FixtureRequest) -> Optional[int]:
+def global_seed(request: pytest.FixtureRequest):
     seed = request.config.getoption("--seed")
 
-    if seed is None or seed == 'None':
-        print("No seed is provided, using the default random behavior.")
-        return None
-
-    if isinstance(seed, (int, str)):
-        try:
-            seed = int(seed)
-            print(f"\nSetting global random seed to {seed}")
-            return seed
-        except ValueError:
-            print(f"Invalid seed: {seed}. Seed must be a valid integer or None.")
-            return None
+    if seed is None:
+        print("\nNo seed is provided, using the default random behavior.")
     else:
-        print(f"Invalid seed type: {type(seed).__name__}. Seed is an integer or None.")
-        return None
+        print(f"\nSetting global random seed to {seed}.")
+        return seed
