9a256081443e1a0015d682a9e05c32aac2ebf066
Muhammad Attallah <muhammad.attallah@colibritd.com>
2024-10-31 16:24:30 +0100

    test: add a test case for custom basis execution

--- tests/core/instruction/measurement/test_basis.py
+++ tests/core/instruction/measurement/test_basis.py
@@ -1,10 +1,14 @@
-import pytest
-
 import numpy as np
 import numpy.typing as npt
+import pytest
 
+from mpqp import QCircuit
+from mpqp.execution import IBMDevice
+from mpqp.execution.runner import _run_single  # pyright: ignore[reportPrivateUsage]
+from mpqp.gates import *
 from mpqp.measures import (
     Basis,
+    BasisMeasure,
     ComputationalBasis,
     HadamardBasis,
     VariableSizeBasis,
@@ -104,3 +108,32 @@ def test_basis_implementations(
     b.pretty_print()
     captured = capsys.readouterr()
     assert captured.out == result_pp
+
+
+def test_run_with_custom_basis():
+    theoretical_results = {'0': 0.5, '5': 0.5}
+
+    v0 = np.array([1, 0, 0, 0, 0, 0, 0, 0])
+    v1 = np.array([0, 1, 0, 0, 0, 0, 0, 0])
+    v2 = np.array([0, 0, 1, 0, 0, 0, 0, 0])
+    v3 = np.array([0, 0, 0, 1, 0, 0, 0, 0])
+    v4 = np.array([0, 0, 0, 0, 1, 0, 0, 0])
+    v5 = np.array([0, 0, 0, 0, 0, 1, 0, 0])
+    v6 = np.array([0, 0, 0, 0, 0, 0, 1, 0])
+    v7 = np.array([0, 0, 0, 0, 0, 0, 0, 1])
+
+    custom_basis = Basis([v0, v1, v2, v3, v4, v5, v6, v7])
+    circuit = QCircuit([H(0), CNOT(0, 1), SWAP(1, 2), Z(2)])
+    circuit.add(BasisMeasure([0, 1, 2], basis=custom_basis, shots=1024))
+
+    res = _run_single(circuit, IBMDevice.AER_SIMULATOR, {})
+
+    actual_probabilities = {}
+    for sample in res.samples:
+        index = sample.index
+        probability = sample.probability
+        actual_probabilities[index] = probability
+
+    for index, theoretical_prob in theoretical_results.items():
+        actual_prob = actual_probabilities.get(int(index), 0)
+        assert np.isclose(actual_prob, theoretical_prob, atol=0.02)
