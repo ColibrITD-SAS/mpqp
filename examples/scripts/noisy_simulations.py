569c8998109e2d876ad14a312c837753c944e434
Hamsatz <hamza.jaffali@colibritd.com>
2024-11-20 17:27:04 +0100

    fix: update noisy example

--- examples/scripts/noisy_simulations.py
+++ examples/scripts/noisy_simulations.py
@@ -1,7 +1,7 @@
 from mpqp import QCircuit
+from mpqp.measures import BasisMeasure
 from mpqp.gates import *
 from mpqp.noise import *
-from mpqp.measures import BasisMeasure
 from mpqp.execution import *
 
 
@@ -11,7 +11,7 @@ circuit = QCircuit(
 
 circuit.add(Depolarizing(0.2))  # Depolarizing on all qubits
 circuit.add(
-    BitFlip(0.12, targets=[0, 1], gates=[H, Z, X, U])
+    BitFlip(0.12, targets=[0, 1], gates=[H, Rz, U])
 )  # BitFlip on qubit 0 and 1, only on gates H, Z, X,U (only H and U will be affected in the circuit)
 circuit.add(AmplitudeDamping(0.2, 0.9, targets=[2]))
 circuit.add(PhaseDamping(0.1, gates=[SWAP]))
