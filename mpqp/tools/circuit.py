e29a8a44ac2b0321eceedb2256ebd2e4061edce4
Hamsatz <hamza.jaffali@colibritd.com>
2024-11-24 21:41:28 +0100

    chore: put back cast to int

--- mpqp/tools/circuit.py
+++ mpqp/tools/circuit.py
@@ -84,9 +84,7 @@ def random_circuit(
     rng = np.random.default_rng(seed)
 
     if nb_gates is None:
-        nb_gates = rng.integers(5, 10)
-        if TYPE_CHECKING:
-            assert isinstance(nb_gates, int)
+        nb_gates = int(rng.integers(5, 10))
 
     qcircuit = QCircuit(nb_qubits)
     for _ in range(nb_gates):
