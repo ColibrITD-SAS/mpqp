d9bd538a31bd3001f8bb01275aed47c571ea1535
Henri <henri.de.boutray@colibritd.com>
2025-02-25 17:28:40 +0100

    doc: grover example in two formats

--- examples/scripts/Grover-w-CustomGate.py
+++ examples/scripts/Grover-w-CustomGate.py
@@ -1,3 +1,4 @@
+import math
 import sys
 
 import numpy as np
@@ -7,19 +8,18 @@ from mpqp.execution import IBMDevice, run
 from mpqp.gates import *
 
 n = int(sys.argv[1])
-k = int(np.rint(np.pi * np.sqrt(2**n) / 4)) - 1
+k = math.floor(math.pi / (4 * math.asin(math.sqrt(1 / 2**n))))
 oracle = UnitaryMatrix(np.diag([1] * (2**n - 1) + [-1]))
 diffusion = CustomGate(UnitaryMatrix(np.diag([-1] + [1] * (2**n - 1))), list(range(n)))
 qaa = QCircuit(
     H.range(n)
     + sum(
         [
-            [CustomGate(oracle, list(range(n)))] + H.range(n) + [diffusion]
+            [CustomGate(oracle, list(range(n)))] + H.range(n) + [diffusion] + H.range(n)
             for _ in range(k)
         ],
         start=[],
     )
-    + H.range(n)
 )
 
 print(run(qaa, IBMDevice.AER_SIMULATOR))
