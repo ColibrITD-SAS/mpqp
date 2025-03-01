6ae14cde13259ccd47f318d46592129a7919cc67
Hamsatz <hamza.jaffali@colibritd.com>
2025-03-01 09:40:52 +0100

    feat: setting up file for different pauli grouping

--- mpqp/tools/pauli_grouping.py
+++ mpqp/tools/pauli_grouping.py
@@ -1,5 +1,8 @@
 from mpqp.core.instruction.measurement.pauli_string import PauliStringMonomial
 
 
-def fully_commuting_pauli_grouping(monomials: set[PauliStringMonomial]):
+def full_commutation_pauli_grouping(monomials: set[PauliStringMonomial]):
+    pass
+
+def qubit_wise_commutation_pauli_grouping(monomials: set[PauliStringMonomial]):
     pass
