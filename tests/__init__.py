66fda21cbe168364e18baf63a90a4c3b7f2bf00e
JulienCalistoTD <164006610+JulienCalistoTD@users.noreply.github.com>
2025-03-12 16:12:29 +0100

    fix: simplify string representation in PauliString and improve code formatting

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,3 @@
 from tests.local_storage.test_local_storage import (
-    create_test_local_storage,
-)  # pyright: ignore[reportUnusedImport]
+    create_test_local_storage,  # pyright: ignore[reportUnusedImport]
+)
