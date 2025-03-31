9158ad36752a610ec5de0d6aa6c72f1f280190f4
Muhammad Attallah <muhammad.attallah@colibritd.com>
2025-03-31 16:34:06 +0200

    fix: correct eigen_values

--- mpqp/core/instruction/measurement/pauli_string.py
+++ mpqp/core/instruction/measurement/pauli_string.py
@@ -861,7 +861,8 @@ class PauliStringMonomial(PauliString):
     def eigen_values(self) -> npt.NDArray[np.bool_]:
         """Return the eigen values associated with this Pauli monomial,
         stored as booleans for efficiency."""
-        return reduce(np.kron, [a.eigen_values > 0 for a in self.atoms])
+        eigvals = reduce(np.kron, [a.eigen_values for a in self.atoms])
+        return eigvals > 0
 
     def __str__(self):
         coef = format_element(self.coef)
