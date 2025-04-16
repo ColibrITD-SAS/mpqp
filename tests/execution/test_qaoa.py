b04a9304f5911e774fcc1cc5c008ebb9fc411e6f
ThomasB-ColibriTD <thomas.benzino@colibritd.com>
2025-04-16 14:12:48 +0200

    chore: test_qaoa in long tests + TSP test fixed

--- tests/execution/test_qaoa.py
+++ tests/execution/test_qaoa.py
@@ -1,6 +1,7 @@
 import pytest
 from mpqp.execution.vqa.qaoa import qaoa_solver, MixerType
 from mpqp.execution.vqa.qubo import Qubo
+import sys
 
 x = Qubo('x')
 y = Qubo('y')
@@ -33,8 +34,8 @@ x2_1 = Qubo('x2_1')
             MixerType.MIXER_X,
             "101",
         ),
-        (2 * x + y + 3 * x + 3 * z, 3, MixerType.MIXER_X, "000"),
-        (3 * x - 2 * y + 100 * (x & y), 4, MixerType.MIXER_X, "01"),
+        (2 * x + y + 3 * x + 4 * z, 3, MixerType.MIXER_X, "000"),
+        (3 * x - 2 * y + 100 * (x & y), 3, MixerType.MIXER_X, "01"),
         (3 * x + 2 * y - 100 * (x & y), 2, MixerType.MIXER_X, "11"),
         (3 * x - 2 * y + 100 * (x | y), 2, MixerType.MIXER_X, "00"),
         (
@@ -50,22 +51,17 @@ x2_1 = Qubo('x2_1')
             "010",
         ),
         (
-            2 * x0_1
-            + x0_2
-            + 2 * x1_0
-            + 4 * x1_2
-            + x2_0
-            + x2_1
-            - 100 * ((x0_1 ^ x2_1) + (x1_0 ^ x2_0) + (x1_2 ^ x0_2))
-            - 100 * ((x0_1 ^ x1_0) + (x0_2 ^ x2_0) + (x1_2 ^ x2_1))
-            - 100 * ((x0_1 ^ x0_2) + (x1_0 ^ x1_2) + (x2_0 ^ x2_1)),
-            3,
+            2 * x0_1 + x0_2 + 2 * x1_0 + 4 * x1_2 + x2_0 + x2_1
+            - 10 * ((x0_1 ^ x2_1) + (x1_0 ^ x2_0) + (x1_2 ^ x0_2))
+            - 10 * ((x0_1 ^ x0_2) + (x1_0 ^ x1_2) + (x2_0 ^ x2_1)),
+            5,
             MixerType.MIXER_X,
-            "010101",
+            "011001",
         ),
     ],
 )
-def test_qaoa(expr: Qubo, depth: int, mixer: MixerType, state: str):
+def qaoa(expr: Qubo, depth: int, mixer: MixerType, state: str):
+    assert qaoa_solver(expr, depth, mixer) == state
 
-    res = qaoa_solver(expr, depth, mixer)
-    assert res == state
+if "--long-local" in sys.argv or "--long" in sys.argv:
+    test_qaoa = qaoa
