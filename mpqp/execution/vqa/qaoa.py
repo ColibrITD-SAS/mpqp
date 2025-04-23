a7cf33e26dd9aeb474d8b11c9ce935efd7b3c394
ThomasB-ColibriTD <thomas.benzino@colibritd.com>
2025-04-23 17:52:53 +0200

    feat: runner device in parameter

--- mpqp/execution/vqa/qaoa.py
+++ mpqp/execution/vqa/qaoa.py
@@ -11,7 +11,7 @@ import scipy.linalg
 import scipy.optimize
 
 from mpqp import QCircuit
-from mpqp.execution import IBMDevice, Result, run
+from mpqp.execution import AvailableDevice, Result, run
 from mpqp.execution.vqa.qubo import Qubo
 from mpqp.gates import CustomGate, H, UnitaryMatrix
 from mpqp.measures import BasisMeasure, ExpectationMeasure, Observable
@@ -24,11 +24,56 @@ class MixerType(Enum):
     MIXER_Z = auto()
 
 
+def qaoa_solver(
+    problem: Qubo, depth: int, type: MixerType, device: AvailableDevice, optimizer: str
+) -> str:
+    """This function solves decision problems using QAOA, the problem needs to
+    be inputted as a QUBO expression.
+
+    Args:
+        problem: QUBO expression representing the problem.
+        depth: The number of cost/mixer gates used in the circuit, the total depth of the ansatz being 2*depth.
+        type: Type of the Mixer Hamiltonian to be used.
+        device: The device that will be used to run the ansatz.
+        optimizer: The optimizer used to minimize. 'Powell' is recommended for better results, but other can be more efficient on specific use cases.
+
+    Returns:
+        A string representing the resulting state of the QAOA optimization.
+
+    Examples:
+        >>> x0 = QuboAtom('x0')
+        >>> x1 = QuboAtom('x1')
+        >>> expr = -3*x0 - 5*x1 + 3*(x0 & x1)
+        >>> qaoa_solver(expr, 4, MixerType.MIXER_X, IBMDevice.AER_SIMULATOR, 'Powell')
+        '01'
+    """
+    observable = problem.to_cost_hamiltonian()
+    problem_size = len(problem)
+    mixer = _generate_mixer_hamiltonian(problem_size, type)
+
+    loss_optimize = partial(
+        _loss, cost=observable, nqubit=problem_size, mixer=mixer, device=device
+    )
+    optimal_params = scipy.optimize.minimize(
+        fun=loss_optimize, method=optimizer, x0=np.zeros(depth * 2)
+    )
+
+    circuit = _generate_ansatz(optimal_params.x, observable, problem_size, mixer)
+    circuit.add(BasisMeasure(list(range(circuit.nb_qubits))))
+
+    result = run(circuit, device)
+    if TYPE_CHECKING:
+        assert isinstance(result, Result)
+    res = str(np.binary_repr(result.counts.index(max(result.counts))))
+    return res.zfill(problem_size)
+
+
 def _loss(
     parameters: list[float],
     cost: Observable,
     nqubit: int,
     mixer: npt.NDArray[np.complex128],
+    device: AvailableDevice,
 ) -> float:
     """Loss function calculating the expectation value of a QAOA ansatz
     initialized with the parameters.
@@ -38,73 +83,18 @@ def _loss(
         cost: The cost Hamiltonian of the ansatz.
         nqubit: The size of the circuit.
         mixer: Mixer hamiltonian of the ansatz.
+        device: The device that will be used to run the ansatz.
 
     Returns:
         A float containing the expectation value of the ansatz.
     """
     circuit = _generate_ansatz(parameters, cost, nqubit, mixer)
     circuit.add(ExpectationMeasure(cost, shots=0))
-    result = run(circuit, IBMDevice.AER_SIMULATOR)
+    result = run(circuit, device)
     if TYPE_CHECKING:
         assert isinstance(result, Result)
         assert isinstance(result.expectation_values, float)
     return result.expectation_values
-
-
-def qaoa_solver(problem: Qubo, depth: int, type: MixerType, optimizer: str) -> str:
-    """This function solves decision problems using QAOA, the problem needs to
-    be inputted as a QUBO expression.
-
-    Args:
-        problem: QUBO expression representing the problem.
-        depth: The number of cost/mixer gates used in the circuit, the total depth of the ansatz being 2*depth.
-        type: Type of the Mixer Hamiltonian to be used.
-
-    Returns:
-        A string representing the resulting state of the QAOA optimization.
-
-    Examples:
-        >>> x0 = QuboAtom('x0')
-        >>> x1 = QuboAtom('x1')
-        >>> expr = -3*x0 - 5*x1 + 3*(x0 & x1)
-        >>> qaoa_solver(expr, 4, MixerType.MIXER_X, 'Powell')
-        '01'
-    """
-    observable = problem.to_cost_hamiltonian()
-    problem_size = len(problem)
-    mixer = _generate_mixer_hamiltonian(problem_size, type)
-
-    loss_optimize = partial(_loss, cost=observable, nqubit=problem_size, mixer=mixer)
-    optimal_params = scipy.optimize.minimize(
-        fun=loss_optimize, method=optimizer, x0=np.zeros(depth * 2)
-    )
-
-    circuit = _generate_ansatz(optimal_params.x, observable, problem_size, mixer)
-    circuit.add(BasisMeasure(list(range(circuit.nb_qubits))))
-
-    result = run(circuit, IBMDevice.AER_SIMULATOR)
-    if TYPE_CHECKING:
-        assert isinstance(result, Result)
-    res = str(np.binary_repr(result.counts.index(max(result.counts))))
-    return res.zfill(problem_size)
-
-
-def _apply_unitary(
-    circuit: QCircuit, operator: Matrix | npt.NDArray[np.complex128], parameter: float
-):
-    """Apply the cost hamiltonian or the mixer hamiltonian to the generated
-    ansatz.
-
-    Args:
-        circuit: Generated Ansatz on which the unitary matrix will me applied.
-        operator: Either the cost hamiltonian or the mixer hamiltonian.
-        parameter: The parameter used to create the unitary matrix.
-    """
-    unitary = scipy.linalg.expm(-1j * parameter * operator)
-    unitary_gate = CustomGate(
-        UnitaryMatrix(unitary.astype(np.complex128)), list(range(circuit.nb_qubits))
-    )
-    circuit.add(unitary_gate)
 
 
 def _generate_mixer_hamiltonian(
@@ -139,6 +129,24 @@ def _generate_mixer_hamiltonian(
             current = np.kron(current, identity)
         result += current
     return result
+
+
+def _apply_unitary(
+    circuit: QCircuit, operator: Matrix | npt.NDArray[np.complex128], parameter: float
+):
+    """Apply the cost hamiltonian or the mixer hamiltonian to the generated
+    ansatz.
+
+    Args:
+        circuit: Generated Ansatz on which the unitary matrix will me applied.
+        operator: Either the cost hamiltonian or the mixer hamiltonian.
+        parameter: The parameter used to create the unitary matrix.
+    """
+    unitary = scipy.linalg.expm(-1j * parameter * operator)
+    unitary_gate = CustomGate(
+        UnitaryMatrix(unitary.astype(np.complex128)), list(range(circuit.nb_qubits))
+    )
+    circuit.add(unitary_gate)
 
 
 def _generate_ansatz(
--- tests/execution/test_qaoa.py
+++ tests/execution/test_qaoa.py
@@ -2,8 +2,9 @@ import sys
 
 import pytest
 
+from mpqp.execution import AvailableDevice, IBMDevice
 from mpqp.execution.vqa.qaoa import MixerType, qaoa_solver
-from mpqp.execution.vqa.qubo import *
+from mpqp.execution.vqa.qubo import Qubo, QuboAtom
 
 x = QuboAtom('x')
 y = QuboAtom('y')
@@ -18,16 +19,17 @@ x2_1 = QuboAtom('x2_1')
 
 
 @pytest.mark.parametrize(
-    "expr, depth, mixer, optimizer, state",
+    "expr, depth, mixer, device, optimizer, state",
     [
-        (2 * x, 2, MixerType.MIXER_X, "Powell", "0"),
-        (x * 2 + 2, 2, MixerType.MIXER_X, "Powell", "0"),
-        (x * 2 + 3 * y, 2, MixerType.MIXER_X, "Powell", "00"),
-        (x * 2 - 3 * y, 2, MixerType.MIXER_X, "Powell", "01"),
+        (2 * x, 2, MixerType.MIXER_X, IBMDevice.AER_SIMULATOR, "Powell", "0"),
+        (x * 2 + 2, 2, MixerType.MIXER_X, IBMDevice.AER_SIMULATOR, "Powell", "0"),
+        (x * 2 + 3 * y, 2, MixerType.MIXER_X, IBMDevice.AER_SIMULATOR, "Powell", "00"),
+        (x * 2 - 3 * y, 2, MixerType.MIXER_X, IBMDevice.AER_SIMULATOR, "Powell", "01"),
         (
             3 * x * y - 4 * x - 2 * y,
             2,
             MixerType.MIXER_X,
+            IBMDevice.AER_SIMULATOR,
             "Powell",
             "10",
         ),
@@ -35,17 +37,47 @@ x2_1 = QuboAtom('x2_1')
             3 * x * y - 4 * x - 2 * y - 3 * z + 1,
             3,
             MixerType.MIXER_X,
+            IBMDevice.AER_SIMULATOR,
             "Powell",
             "101",
         ),
-        (2 * x + y + 3 * x + 4 * z, 3, MixerType.MIXER_X, "Powell", "000"),
-        (3 * x - 2 * y + 100 * (x & y), 3, MixerType.MIXER_X, "Powell", "01"),
-        (3 * x + 2 * y - 100 * (x & y), 2, MixerType.MIXER_X, "Powell", "11"),
-        (3 * x - 2 * y + 100 * (x | y), 2, MixerType.MIXER_X, "Powell", "00"),
+        (
+            2 * x + y + 3 * x + 4 * z,
+            3,
+            MixerType.MIXER_X,
+            IBMDevice.AER_SIMULATOR,
+            "Powell",
+            "000",
+        ),
+        (
+            3 * x - 2 * y + 100 * (x & y),
+            3,
+            MixerType.MIXER_X,
+            IBMDevice.AER_SIMULATOR,
+            "Powell",
+            "01",
+        ),
+        (
+            3 * x + 2 * y - 100 * (x & y),
+            2,
+            MixerType.MIXER_X,
+            IBMDevice.AER_SIMULATOR,
+            "Powell",
+            "11",
+        ),
+        (
+            3 * x - 2 * y + 100 * (x | y),
+            2,
+            MixerType.MIXER_X,
+            IBMDevice.AER_SIMULATOR,
+            "Powell",
+            "00",
+        ),
         (
             3 * x * y - 4 * x - 2 * y - 3 * z + 100 * (x | z),
             3,
             MixerType.MIXER_X,
+            IBMDevice.AER_SIMULATOR,
             "Powell",
             "010",
         ),
@@ -53,6 +85,7 @@ x2_1 = QuboAtom('x2_1')
             -10 * x - 100 * y + 100 * z - 1000 * (x ^ y),
             4,
             MixerType.MIXER_X,
+            IBMDevice.AER_SIMULATOR,
             "Powell",
             "010",
         ),
@@ -67,13 +100,21 @@ x2_1 = QuboAtom('x2_1')
             - 10 * ((x0_1 ^ x0_2) + (x1_0 ^ x1_2) + (x2_0 ^ x2_1)),
             5,
             MixerType.MIXER_X,
+            IBMDevice.AER_SIMULATOR,
             "Nelder-Mead",
             "011001",
         ),
     ],
 )
-def qaoa(expr: Qubo, depth: int, mixer: MixerType, optimizer: str, state: str):
-    assert qaoa_solver(expr, depth, mixer, optimizer) == state
+def qaoa(
+    expr: Qubo,
+    depth: int,
+    mixer: MixerType,
+    device: AvailableDevice,
+    optimizer: str,
+    state: str,
+):
+    assert qaoa_solver(expr, depth, mixer, device, optimizer) == state
 
 
 if "--long-local" in sys.argv or "--long" in sys.argv:
