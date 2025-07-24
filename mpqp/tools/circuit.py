e398fe50fbff5c725919e958028f6d7c8f3a5440
ThomasBenzino <thomas.benzino@colibritd.com>
2025-07-24 16:22:31 +0200

    fix: typechecking

--- mpqp/tools/circuit.py
+++ mpqp/tools/circuit.py
@@ -96,7 +96,7 @@ def random_circuit(
 def statevector_from_random_circuit(
     nb_qubits: int = 5,
     seed: Optional[int] = None,
-) -> npt.NDArray[np.complex64]:
+) -> npt.NDArray[np.complex128]:
     """
     This function creates a statevector with a specified number of qubits,
     generated from a random circuit executed on IBM AER Simulator.
