0be003749cbd23a4891aa05212a5a73909daf2e4
Muhammad Attallah <muhammad.attallah@colibritd.com>
2026-02-23 16:53:05 +0100

    chore: update _version.py

--- mpqp/_version.py
+++ mpqp/_version.py
@@ -1,3 +1,7 @@
+"""Package version for mpqp.
+Release version values are managed by ``setuptools_scm`` from Git tags
+"""
+
 from __future__ import annotations
 
 from importlib.metadata import PackageNotFoundError, version
@@ -5,4 +9,4 @@ from importlib.metadata import PackageNotFoundError, version
 try:
     __version__ = version("mpqp")
 except PackageNotFoundError:
-    __version__ = "0.0.0"
+    __version__ = "0.0.0+unknown"
