14b0495801fba3a2bd55f39b7cc46a6a9e20a91e
Mathieu Gras <mathieu.gras@colibritd.com>
2026-03-11 10:36:20 +0100

    fix: ibm warnings

--- mpqp/execution/providers/ibm.py
+++ mpqp/execution/providers/ibm.py
@@ -23,7 +23,6 @@ from mpqp.execution.result import Result, Sample, StateVector
 from mpqp.noise import DimensionalNoiseModel
 from mpqp.tools.errors import (
     DeviceJobIncompatibleError,
-    IBMNoiseModelGeneration,
     IBMRemoteExecutionError,
     InstructionParsingError,
 )
@@ -222,8 +221,6 @@ def generate_qiskit_noise_model(
         The qubit order in the returned noise model is reversed to match
         ``qiskit``'s qubit ordering conventions.
     """
-    import io
-    import logging
 
     from qiskit_aer.noise import NoiseModel as Qiskit_NoiseModel
 
@@ -250,85 +247,34 @@ def generate_qiskit_noise_model(
 
     noisy_identity_counter = 0
 
-    log_stream = io.StringIO()
-    log_handler = logging.StreamHandler(log_stream)
-    log_handler.setLevel(logging.WARNING)
+    for noise in modified_circuit.noises:
+        qiskit_error = noise.to_other_language(Language.QISKIT)
+        if TYPE_CHECKING:
+            from qiskit_aer.noise.errors.quantum_error import QuantumError
 
-    logger = logging.getLogger()
-    logger.addHandler(log_handler)
-    try:
-        for noise in modified_circuit.noises:
-            qiskit_error = noise.to_other_language(Language.QISKIT)
-            if TYPE_CHECKING:
-                from qiskit_aer.noise.errors.quantum_error import QuantumError
+            assert isinstance(qiskit_error, QuantumError)
 
-                assert isinstance(qiskit_error, QuantumError)
+        # If all qubits are affected
+        if len(noise.targets) == modified_circuit.nb_qubits:
+            if len(noise.gates) != 0:
+                for gate in noise.gates:
+                    size = gate.nb_qubits
+                    if TYPE_CHECKING:
+                        assert isinstance(size, int)
 
-            # If all qubits are affected
-            if len(noise.targets) == modified_circuit.nb_qubits:
-                if len(noise.gates) != 0:
-                    for gate in noise.gates:
-                        size = gate.nb_qubits
-                        if TYPE_CHECKING:
-                            assert isinstance(size, int)
-
-                        if isinstance(noise, DimensionalNoiseModel):
-                            if size == noise.dimension:
-                                noise_model.add_all_qubit_quantum_error(
-                                    qiskit_error, [gate.qiskit_string]
-                                )
-                        else:
-                            tensor_error = qiskit_error
-                            for _ in range(1, size):
-                                tensor_error = tensor_error.tensor(qiskit_error)
+                    if isinstance(noise, DimensionalNoiseModel):
+                        if size == noise.dimension:
                             noise_model.add_all_qubit_quantum_error(
-                                tensor_error, [gate.qiskit_string]
+                                qiskit_error, [gate.qiskit_string], warnings=False
                             )
-                else:
-                    for gate in gate_instructions:
-
-                        if not isinstance(gate, NativeGate):
-                            warnings.warn(
-                                f"Ignoring gate '{type(gate)}' as it's not a native gate. "
-                                "Noise is only applied to native gates."
-                            )
-                            continue
-
-                        connections = gate.connections()
-                        size = len(connections)
-
-                        reversed_qubits = [
-                            modified_circuit.nb_qubits - 1 - qubit
-                            for qubit in connections
-                        ]
-
-                        if (
-                            isinstance(noise, DimensionalNoiseModel)
-                            and noise.dimension > size
-                        ):
-                            continue
-                        elif (
-                            isinstance(noise, DimensionalNoiseModel)
-                            and 1 < noise.dimension == size
-                        ):
-                            noise_model.add_quantum_error(
-                                qiskit_error,
-                                [gate.qiskit_string],
-                                reversed_qubits,
-                            )
-                        else:
-                            tensor_error = qiskit_error
-                            for _ in range(1, size):
-                                tensor_error = tensor_error.tensor(qiskit_error)
-                            noise_model.add_quantum_error(
-                                tensor_error,
-                                [gate.qiskit_string],
-                                reversed_qubits,
-                            )
-
+                    else:
+                        tensor_error = qiskit_error
+                        for _ in range(1, size):
+                            tensor_error = tensor_error.tensor(qiskit_error)
+                        noise_model.add_all_qubit_quantum_error(
+                            tensor_error, [gate.qiskit_string], warnings=False
+                        )
             else:
-                gates_str = [gate.qiskit_string for gate in noise.gates]
-
                 for gate in gate_instructions:
 
                     if not isinstance(gate, NativeGate):
@@ -338,84 +284,112 @@ def generate_qiskit_noise_model(
                         )
                         continue
 
-                    # If gates are specified in the noise and the current gate is not in the list, we move to the next one
-                    if len(gates_str) != 0 and gate.qiskit_string not in gates_str:
-                        continue
-
                     connections = gate.connections()
-                    intersection = connections.intersection(set(noise.targets))
+                    size = len(connections)
 
-                    # Gate targets are included in the noise targets
-                    if intersection == connections:
+                    reversed_qubits = [
+                        modified_circuit.nb_qubits - 1 - qubit for qubit in connections
+                    ]
 
-                        reversed_qubits = [
-                            modified_circuit.nb_qubits - 1 - qubit
-                            for qubit in connections
-                        ]
+                    if (
+                        isinstance(noise, DimensionalNoiseModel)
+                        and noise.dimension > size
+                    ):
+                        continue
+                    elif (
+                        isinstance(noise, DimensionalNoiseModel)
+                        and 1 < noise.dimension == size
+                    ):
+                        noise_model.add_quantum_error(
+                            qiskit_error,
+                            [gate.qiskit_string],
+                            reversed_qubits,
+                            warnings=False,
+                        )
+                    else:
+                        tensor_error = qiskit_error
+                        for _ in range(1, size):
+                            tensor_error = tensor_error.tensor(qiskit_error)
+                        noise_model.add_quantum_error(
+                            tensor_error,
+                            [gate.qiskit_string],
+                            reversed_qubits,
+                            warnings=False,
+                        )
 
-                        # Noise model is multi-dimensional
-                        if isinstance(
-                            noise, DimensionalNoiseModel
-                        ) and noise.dimension > len(connections):
-                            continue
-                        elif isinstance(
-                            noise, DimensionalNoiseModel
-                        ) and 1 < noise.dimension == len(connections):
+        else:
+            gates_str = [gate.qiskit_string for gate in noise.gates]
+
+            for gate in gate_instructions:
+
+                if not isinstance(gate, NativeGate):
+                    warnings.warn(
+                        f"Ignoring gate '{type(gate)}' as it's not a native gate. "
+                        "Noise is only applied to native gates."
+                    )
+                    continue
+
+                # If gates are specified in the noise and the current gate is not in the list, we move to the next one
+                if len(gates_str) != 0 and gate.qiskit_string not in gates_str:
+                    continue
+
+                connections = gate.connections()
+                intersection = connections.intersection(set(noise.targets))
+
+                # Gate targets are included in the noise targets
+                if intersection == connections:
+
+                    reversed_qubits = [
+                        modified_circuit.nb_qubits - 1 - qubit for qubit in connections
+                    ]
+
+                    # Noise model is multi-dimensional
+                    if isinstance(
+                        noise, DimensionalNoiseModel
+                    ) and noise.dimension > len(connections):
+                        continue
+                    elif isinstance(
+                        noise, DimensionalNoiseModel
+                    ) and 1 < noise.dimension == len(connections):
+                        noise_model.add_quantum_error(
+                            qiskit_error,
+                            [gate.qiskit_string],
+                            reversed_qubits,
+                            warnings=False,
+                        )
+                    else:
+                        tensor_error = qiskit_error
+                        for _ in range(1, len(connections)):
+                            tensor_error = tensor_error.tensor(qiskit_error)
+                        noise_model.add_quantum_error(
+                            tensor_error,
+                            [gate.qiskit_string],
+                            reversed_qubits,
+                            warnings=False,
+                        )
+
+                # Only some targets of the gate are included in the noise targets
+                elif len(intersection) != 0:
+                    if (not isinstance(noise, DimensionalNoiseModel)) or (
+                        noise.dimension == 1
+                    ):
+                        for qubit in intersection:
+                            # We add a custom identity gate on the relevant
+                            # qubits to apply noise after the gate
+                            labeled_identity = Id(
+                                target=qubit,
+                                label=f"noisy_identity_{noisy_identity_counter}",
+                            )
                             noise_model.add_quantum_error(
                                 qiskit_error,
-                                [gate.qiskit_string],
-                                reversed_qubits,
+                                [labeled_identity.label],
+                                [modified_circuit.nb_qubits - 1 - qubit],
                             )
-                        else:
-                            tensor_error = qiskit_error
-                            for _ in range(1, len(connections)):
-                                tensor_error = tensor_error.tensor(qiskit_error)
-                            noise_model.add_quantum_error(
-                                tensor_error,
-                                [gate.qiskit_string],
-                                reversed_qubits,
+                            gate_index = modified_circuit.instructions.index(gate)
+                            modified_circuit.instructions.insert(
+                                gate_index + 1, labeled_identity
                             )
-
-                    # Only some targets of the gate are included in the noise targets
-                    elif len(intersection) != 0:
-                        if (not isinstance(noise, DimensionalNoiseModel)) or (
-                            noise.dimension == 1
-                        ):
-                            for qubit in intersection:
-                                # We add a custom identity gate on the relevant
-                                # qubits to apply noise after the gate
-                                labeled_identity = Id(
-                                    target=qubit,
-                                    label=f"noisy_identity_{noisy_identity_counter}",
-                                )
-                                noise_model.add_quantum_error(
-                                    qiskit_error,
-                                    [labeled_identity.label],
-                                    [modified_circuit.nb_qubits - 1 - qubit],
-                                )
-                                gate_index = modified_circuit.instructions.index(gate)
-                                modified_circuit.instructions.insert(
-                                    gate_index + 1, labeled_identity
-                                )
-                                noisy_identity_counter += 1
-
-        log_handler.flush()
-        captured_logs = log_stream.getvalue()
-
-        if (
-            multiple_noise_warning is False
-            and "WARNING: quantum error already exists" in captured_logs
-        ):
-            pass
-        elif len(captured_logs) != 0:
-            warnings.warn(
-                "Some warnings were raised during the generation of the noise model:\n"
-                f"{captured_logs}",
-                IBMNoiseModelGeneration,
-            )
-
-    finally:
-        logger.removeHandler(log_handler)
+                            noisy_identity_counter += 1
 
     return noise_model, modified_circuit
 
