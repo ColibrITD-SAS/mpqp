7077007e3f0a6aecb213bdeace3d0e8633e757fd
Mathieu Gras <mathieu.gras@colibritd.com>
2026-09-22 12:02:22 +0200

    fix: deleted file

--- zz.py
+++
@@ -1,8 +0,0 @@
-from mpqp import *
-from mpqp.core.circuitbinding import CircuitBinding, BindingMode
-
-cb = CircuitBinding(
-    [QCircuit([X(0), BasisMeasure()]), QCircuit([Z(0), BasisMeasure()])], shots=1
-)
-
-print(run(cb, AWSDevice.IQM_EMERALD))
